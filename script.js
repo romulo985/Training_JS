5c175db423dfd62dd6fb873b15ccd6317feb736f
Romulo <romuloprog@gmail.com>
2022-04-16 15:00:32 -0400

    Alteração de numero entre variaveis

---
+++ script.js
@@ -0,0 +1,33 @@
+/* let a = 'vermelho';
+let b = 'azul';
+let c = null;
+
+console.log('numero a : ',a);
+console.log('numero b : ',b);
+
+c = a;
+a = b;
+b = c;
+
+console.log(' numero alterado de a:',a);
+console.log(' numero alterado de b :',b);
+
+console.log('numero c eh :',c); */
+
+let a = 'vermelho';
+let b = 'azul';
+
+console.log('numero a : ',a);
+console.log('numero b : ',b)
+
+let c = a;
+a = b;
+b = c;
+
+console.log(' numero alterado de a:',a);
+console.log(' numero alterado de b :',b);
+
+;
+
+
+
